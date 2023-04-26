2c9c4b858e195875dd3075d936564b2611c8197e
Adam Driscoll <adam.r.driscoll@gmail.com>
2023-04-26 13:15:32 -0500

    Fix scripts.ps1

--- Notifications/Universal.Notifications/.universal/scripts.ps1
+++ Notifications/Universal.Notifications/.universal/scripts.ps1
@@ -1,3 +1,3 @@
-New-PSUScript -Module "Universal.Notifications" -Command "Send-PSUFailedJobEmail" -Description "Sends an email when a job fails." 
-New-PSUScript -Module "Universal.Notifications" -Command "Send-PSUFailedJobSlackNotification" -Description "Sends a Slack notification when a job fails." 
-New-PSUScript -Module "Universal.Notifications" -Command "Send-PSUFailedJobTeamsNotification" -Description "Sends a Microsoft Teams notification when a job fails." 
+New-PSUScript -Name "Send-PSUFailedJobEmail" -Module "Universal.Notifications" -Command "Send-PSUFailedJobEmail" -Description "Sends an email when a job fails." 
+New-PSUScript -Name "Send-PSUFailedJobSlackNotification" -Module "Universal.Notifications" -Command "Send-PSUFailedJobSlackNotification" -Description "Sends a Slack notification when a job fails." 
+New-PSUScript -Name "Send-PSUFailedJobTeamsNotification" -Module "Universal.Notifications" -Command "Send-PSUFailedJobTeamsNotification" -Description "Sends a Microsoft Teams notification when a job fails." 
